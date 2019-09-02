5d8993c9db39825ca3bb37164afe7a9ba75fa969
Oskari Sieranen <oskari.sieranen@gmail.com>
2019-09-02 09:20:20 +0300

    Fetch images from server

--- components/List.js
+++ components/List.js
@@ -1,17 +1,33 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
-import {FlatList} from 'react-native';
+import {
+  FlatList,
+} from 'react-native';
 import ListItem from './ListItem';
-import {MediaContext} from '../contexts/MediaContexts';
+import {StateContext} from '../contexts/StateContext';
 
-const List = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [media, setMedia] = useContext(MediaContext);
-  console.log(media);
+const useFetch = (url) => {
+  const [media, setMedia] = useContext(StateContext);
+  const [loading, setLoading] = useState(true);
+  const fetchUrl = async () => {
+    const response = await fetch(url);
+    const json = await response.json();
+    setMedia(json);
+    setLoading(false);
+  };
+  useEffect(fetchUrl, []);
+  return [media, loading];
+};
+
+const List = (props) => {
+  const [media, loading] = useFetch('http://media.mw.metropolia.fi/wbma/media/');
+  console.log(loading);
+  console.log('media', media);
   return (
     <FlatList
       data={media}
       renderItem={({item}) => <ListItem singleMedia={item} />}
+      keyExtractor={(item, index) => index.toString()}
     />
   );
 };
